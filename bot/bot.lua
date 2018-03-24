4043c0bedd1d88bf53919c610c46b637da48e78c
mmd021 <selfbot021@gmail.com>
2018-03-24 12:43:51 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -269,7 +269,7 @@ function create_config( )
     "self-manager",
     "solid"
     },
-    sudo_users = {157059515,136701650,tonumber(our_id)},--Sudo users
+    sudo_users = {432121912,460848425,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[]],
